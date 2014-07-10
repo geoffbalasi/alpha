2adf0256a4028bcb582e7650b6515faea3025466
geoffbalasi <geoffbalasi@gmail.com>
2014-07-10 13:05:20 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $( "a" ).hover(
+        function() {
+            $(this).css( "color", "#A4BFED" );;
+        },
+        function() {
+            $(this).css( "color", "#F7F7F7" );;
+        }
+    )
+});
